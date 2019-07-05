45ad178ce4fac3a03003044b0c9634d69d7e03b0
Emanuell Santos <dfmanu06@gmail.com>
2019-07-05 00:56:00 -0300

    Criacao da classe NegociacoesView + template dinamico

---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,13 @@
+class Negociacoes {
+
+    private _negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao): void {
+        this._negociacoes.push(negociacao);
+    }
+
+    getArray(): Negociacao[] {
+        return [].concat(this._negociacoes);
+    }
+
+}
